41c56573715414023025163ba539292a9bce4484
Vinod kumar <vinodstm74@gmail.com>
2024-10-03 20:25:53 +0530

    new file added

--- com.core.constucturtor.spring/src/main/java/spring/App/App.java
+++ com.core.constucturtor.spring/src/main/java/spring/App/App.java
@@ -1,25 +1,19 @@
-package spring.core;
+package spring.App;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import spring.core.Entity.Employee;
+import spring.Emp.Employee;
 
 public class App {
 	
 	public static void main(String args[])
 	{
 		
-		ApplicationContext ioc=new ClassPathXmlApplicationContext("ApplicationContexxt.xml");
-		
-		Employee bean=  (Employee) ioc.getBean("emp");
+		ApplicationContext ioc=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+	Employee bean=	ioc.getBean(Employee.class,"emp2");
 		
 		System.out.println(bean);
-		
-		
-		
-		
-		
 	}
 
 }
--- com.core.constucturtor.spring/src/main/java/spring/Emp/Employee.java
+++ com.core.constucturtor.spring/src/main/java/spring/Emp/Employee.java
@@ -1,4 +1,4 @@
-package spring.core.Entity;
+package spring.Emp;
 
 public class Employee {
 	
@@ -6,47 +6,110 @@ public class Employee {
 	private String Fname;
 	private String Lname;
 	private String Gender;
+	private int saraly;
+	
+	
 	
 	
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Employee(int id, String fname, String lname, String gender) {
+
+
+
+
+	public Employee(int id, String fname, String lname, String gender, int saraly) {
 		super();
 		this.id = id;
 		Fname = fname;
 		Lname = lname;
 		Gender = gender;
+		this.saraly = saraly;
 	}
+
+
+
+
 	public int getId() {
 		return id;
 	}
+
+
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
+
+
 	public String getFname() {
 		return Fname;
 	}
+
+
+
+
 	public void setFname(String fname) {
 		Fname = fname;
 	}
+
+
+
+
 	public String getLname() {
 		return Lname;
 	}
+
+
+
+
 	public void setLname(String lname) {
 		Lname = lname;
 	}
+
+
+
+
 	public String getGender() {
 		return Gender;
 	}
+
+
+
+
 	public void setGender(String gender) {
 		Gender = gender;
 	}
+
+
+
+
+	public int getSaraly() {
+		return saraly;
+	}
+
+
+
+
+	public void setSaraly(int saraly) {
+		this.saraly = saraly;
+	}
+
+
+
+
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", Fname=" + Fname + ", Lname=" + Lname + ", Gender=" + Gender + "]";
+		return "Employee [id=" + id + ", Fname=" + Fname + ", Lname=" + Lname + ", Gender=" + Gender + ", saraly="
+				+ saraly + "]";
 	}
+	
+	
+	
+	
 	
 	
 	
